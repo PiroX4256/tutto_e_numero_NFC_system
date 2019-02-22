01550344dd95b72a157a3d386bc911bdd9b842d4
Luca Pirovano <luca.pirovano.98@gmail.com>
2019-02-22 18:57:29 +0100

    Add files via upload

--- Pasti_NFC_System.py
+++ Pasti_NFC_System.py
@@ -1,44 +1,47 @@
+# /// Code entirely realized by Luca Pirovano \\\
+# /// Contact me for your projects \\\
+# /// www.lucapirovano.com || contact@lucapirovano.com \\\
+
 import mysql.connector
 from time import *
 from Tkinter import *
 import nfc
 
+# --- MySQL CONFIGURATION ---
+# --- IMPORTANT: Please insert your MySQL Server login data, in order to establish a connection to the server
+db_host = "13.94.187.146"        #Database hostname
+db_name = "tuttoenumero"        #Database name
+db_user = "tuttoenumero"        #Database login user
+db_password = "giugno98"    #Database login password
+db_table = "Buoni_pasto"    #Table name in MySQL database
+
+# --- END OF MySQL CONFIGURATION ---
+
+# ---*** BOOT PARAMETERS ***--- #
+
 day = input("\n\nPer favore inserire l'ID del pasto. Puoi trovare gli ID corrispondenti qui sotto.\n -----ID----\n 1.MERCOLEDI CENA\n 2.GIOVEDI PRANZO \n 3.GIOVEDI CENA \n 4.VENERDI PRANZO \n 5.VENERDI CENA \n  Scelta: ")
-while day<1||day>5:
-    day = input("Per favore inserire un giorno valido o premere \"q\" per uscire:")
-    if day=='q':
-        quit()
+if day>5:
+    print ("Devi inserire un numero compreso tra 1 e 5. \nIl programma verra' ora automaticamente chiuso.")
+    sleep(3)
+    quit()
+
+elif day<1:
+    print ("Devi inserire un numero compreso tra 1 e 5. \nIl programma verra' ora automaticamente chiuso.")
+    sleep(3)
+    quit()
 
 machine = input("\n\nPer favore inserire l'ID macchina. Puoi trovare gli ID corrispondenti qui sotto.\n ----IDs----\n 1.BANCO PRIMI\n 2.BANCO SECONDI\n 3.FRUTTA / DESSERT\n   Scelta: ")
-while machine<1||machine>3;
-    machine= input("Devi inserire un numero compreso tra 1 e 3, altrimenti premi \"q\" per uscire:")
-    if machine=='q':
-        quit()
+if machine>3:
+    print ("Devi inserire un numero compreso tra 1 e 3. \nIl programma verra' ora automaticamente chiuso.")
+    sleep(3)
+    quit()
+
+elif machine<1:
+    print ("Devi inserire un numero compreso tra 1 e 3. \nIl programma verra' ora automaticamente chiuso.")
+    sleep(3)
+    quit()
 #=============================================================================
-# ----**** GRAPHIC INTERFACE DEFINITION ****---- #
 
-app = Tk()
-app.title("Food Convalidator")
-
-app.geometry("900x800+300+100")
-
-
-#the variable
-bv = StringVar()
-meal = StringVar()
-
-row1 = Label(app, text='Convalidatore', font=("Bahnschrift", 44)).pack(padx = 20, pady = 20)
-
-#the entry box
-b1 = Entry(app, textvariable=bv, font=("Bahnschrift", 44))
-b1.pack(padx = 20, pady = 15)
-b1.focus_force()
-
-status = Label(app, text="IN ATTESA", font=("Bahnschrift", 35), background="#cedbef")
-status.pack(padx = 20, pady = 30)
-
-name = Label(app, text="Braccialetto: ", font=("Bahnschrift", 28))
-name.pack(pady = 5)
 
 
 
@@ -50,7 +53,7 @@ def validate(event):
     bu1.update()
     status.configure(text="AVVICINARE CHIP", background="#8af2fc")
     status.update()
-    db = mysql.connector.connect(user='tuttoenumero', password='giugno98', host='137.74.231.85', database='tuttoenumero')   #Establish a MySQL database connection
+    db = mysql.connector.connect(user=db_user, password=db_password, host=db_host, database=db_name)   #Establish a MySQL database connection
     cless = nfc.ContactlessFrontend("usb")  #Establish a USB connection with the reader device
 
     if day== 1:                             #Link all meals to their definition // used during validation
@@ -80,9 +83,9 @@ def validate(event):
 
     print "Braccialetto: ", ID
     cursor = db.cursor()                    #Create MySQL DB pointer
-    cursor.execute("SELECT Tipo FROM Buoni_Pasto WHERE ID_Bracelet = '%s'" % (ID))  #Execute DB query to fetch meal type (Carnet or Single)
+    cursor.execute("SELECT Tipo FROM %s WHERE ID_Bracelet = '%s'" % (db_table, ID))  #Execute DB query to fetch meal type (Carnet or Single)
     result = cursor.fetchall()              #Fetch results into an array
-
+    
     if machine == 1:                        #Bind machine ID to meal name
         portate = "Primo"
     if machine == 2:
@@ -91,13 +94,13 @@ def validate(event):
         portate = "Dessert"
 
     if cursor.rowcount == 0:                #If bracelet UID is not found in MySQL DB, return an error message
-        status.configure(text="BRACCIALETTO NON REGISTRATO", background="orange")
+        status.configure(text="BRACCIALETTO NON REGISTRATO", background="orange")  
         print ("Non registrato")
         status.update()
         sleep(0.3)
     for row in result:                      #If bracelet UID is foung, continue with device meal validation (in case it's a Carnet meal type)
         if "Carnet" in row[0]:
-            cursor.execute ("SELECT %s FROM Buoni_Pasto WHERE ID_Bracelet = '%s'" % (portate, ID))  #Select meal to check if it's already been taken by customer.
+            cursor.execute ("SELECT %s FROM %s WHERE ID_Bracelet = '%s'" % (portate, db_table, ID))  #Select meal to check if it's already been taken by customer.
             validation = cursor.fetchall()
             if day in validation[0]:        #Check previously validations (as declared above)
                 print("Already Used")
@@ -105,13 +108,13 @@ def validate(event):
                 status.update()
                 sleep(0.3)
             else:                           #if meal has not been taken before, validate the NFC tag checking if it's enabled to that specific meal.
-                cursor.execute("UPDATE Buoni_Pasto SET %s=%s WHERE ID_BRACELET='%s'" % (portate, day, ID))  #Set meal counter to day ID to prevent the "taking-again"
+                cursor.execute("UPDATE %s SET %s=%s WHERE ID_BRACELET='%s'" % (db_table, portate, day, ID))  #Set meal counter to day ID to prevent the "taking-again"
                 db.commit()
                 print("Valid")
                 status.configure (text="CARNET: VALID", background = "green")   #Display valid output
                 status.update()
         else:
-            cursor.execute ("SELECT %s FROM Buoni_Pasto WHERE ID_Bracelet = '%s'" % (portate, ID))  #If coupon is not a "Carnet" type, proceed to single day validation
+            cursor.execute ("SELECT %s FROM %s WHERE ID_Bracelet = '%s'" % (portate, db_table, ID))  #If coupon is not a "Carnet" type, proceed to single day validation
             validation = cursor.fetchall()
             if day in validation[0]:        #Check previously validations (as declared above)
                 print("Already Used")
@@ -120,26 +123,54 @@ def validate(event):
                 sleep(0.3)
             else:                           #if meal has not been taken before, validate the NFC tag checking if it's enabled to that specific meal.
                 db.commit()
-                cursor.execute("SELECT Pasti FROM Buoni_Pasto WHERE ID_Bracelet = '%s'" % (ID))     #Select the single meal table, to check which meal are enabled
+                cursor.execute("SELECT Pasti FROM %s WHERE ID_Bracelet = '%s'" % (db_table, ID))     #Select the single meal table, to check which meal are enabled
                 results = cursor.fetchall()
-                for rows in results:
+                for rows in results:   
                     if kw in rows[0]:       #Check the fetched result, precisely if the present meal is enabled.
-                        cursor.execute("UPDATE Buoni_Pasto SET %s=%s WHERE ID_BRACELET='%s'" % (portate, day, ID))  #Set meal counter to day ID to prevent the "taking-again"
+                        cursor.execute("UPDATE %s SET %s=%s WHERE ID_BRACELET='%s'" % (db_table, portate, day, ID))  #Set meal counter to day ID to prevent the "taking-again"
+                        db.commit()
                         print ("Valid")     #Display valid output
                         status.configure (text="VALID", background = "green")
                         status.update()
 
                     else:
                         print ("Not Valid") #Display invalid output
-                        status.configure (text="NOT VALID", background = "red")
+                        status.configure (text="NOT VALID", background = "red")  
                         status.update()
     cursor.close()                          #Close DB pointer
     db.close()                              #Close DB connection
     clear_textbox()                         #Clear the verification module in order to be able to verify another customer
-
+  
 
 #=============================================================================
-# ----**** GRAPHICS (PART II) ****---- #
+
+# ----**** GRAPHIC INTERFACE DEFINITION ****---- #
+
+app = Tk()
+app.title("Tutto e' Numero - Food Convalidator")
+
+app.geometry("900x800+300+100")
+
+title = Label (app, text="Tutto e' Numero 2019", font=("Bahnschrift", 50), fg="#d34343").pack(padx = 5, pady =0) #Title
+
+row1 = Label(app, text = "Food Convalidation", font=("Bahnschrift", 35), fg="#d34343").pack(padx = 20, pady = 0) #First text row
+
+
+#the variable
+bv = StringVar()
+meal = StringVar()
+
+
+#the entry box
+b1 = Entry(app, textvariable=bv, font=("Bahnschrift", 44))
+b1.pack(padx = 20, pady = 15)
+b1.focus_force()
+
+status = Label(app, text="IN ATTESA", font=("Bahnschrift", 35), background="#cedbef")
+status.pack(padx = 20, pady = 30)
+
+name = Label(app, text="Braccialetto: ", font=("Bahnschrift", 28))
+name.pack(pady = 5)
 
 #b1.bind("<Button-1>", validate)
 b1.bind("<Return>", validate)
@@ -150,7 +181,7 @@ bu1.pack(padx = 20, pady = 20)
 bu1.bind("<Button-1>", validate)
 bu1.bind("<Return>", validate)
 
-quitter = Button(app, text="Quit", font=("Bahnschrift", 30), command = quit)
+quitter = Button(app, text="Esci", font=("Bahnschrift", 30), command = quit)
 quitter.pack(padx = 5, pady = 5)
 
 if day== 1:
